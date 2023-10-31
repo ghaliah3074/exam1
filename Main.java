81aab10ef3559ffe5ea9c33384f2a19fc38165a9
ghaliah3074 <148686086+ghaliah3074@users.noreply.github.com>
2023-10-31 11:07:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner f = new Scanner(System.in);
+//        System.out.println("enter pls num1");
+//        int num1= f.nextInt();
+
+
+//        if ( num1 %==0){
+//            System.out.println("the number is even");
+//
+//        } else
+//            System.out.println("the number is odd");
+//
+//
+//
+//
+//        //2
+
+//        System.out.println(" enter the num: ");
+//        int i=f.nextInt();
+//        int sum = 0;
+//        for (int i=1; i > 0; i++) {
+//
+//
+//            sum = sum + i;
+//
+//            while (sum == 0) {
+//                System.out.println(" the sum is: " + sum);
+//                sum++;
+//            }
+//
+//        }
+
+////        //3
+//        int num[] = {10, -21, 30, 31, -25};
+//        AbstractList<> positive = new ArrayList();
+//        AbstractList<> negative = new ArrayList();
+//
+//        if (num[]>0){
+//            positive.add(num);
+//
+//        }else
+//        negative.add(num);
+//
+//        System.out.println(positive);
+//        System.out.println(negative);
+//        //q4
+//
+//
+//        String original[] ={
+//            "Tuwaiq", "Bootcamp", "Student", "JAVA"
+//        } ;
+//        Arrays.stream(original).min();
+//
+//    }
+//    }
